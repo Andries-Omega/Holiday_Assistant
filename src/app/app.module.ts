63f933e10c958049ba6818bfea8045f4a32174fd
Andries-Omega <achimule@jhb.dvt.co.za>
2022-04-03 17:33:46 +0200

    Added header and other common componenets like scroll, and app guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componenets/common-components/header/header.component';
+import { GuideComponent } from './componenets/common-components/guide/guide.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ScrollUpComponent } from './componenets/common-components/scroll-up/scroll-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    GuideComponent,
+    ScrollUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
