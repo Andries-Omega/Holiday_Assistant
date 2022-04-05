2cba2dace0d5af5ad8ba8323e671b727b35e7005
Andries-Omega <achimule@jhb.dvt.co.za>
2022-04-05 06:42:58 +0200

    Fixed Components file speeling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componenets/common-components/header/header.component';
-import { GuideComponent } from './componenets/common-components/guide/guide.component';
+import { HeaderComponent } from './components/common-components/header/header.component';
+import { GuideComponent } from './components/common-components/guide/guide.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ScrollUpComponent } from './componenets/common-components/scroll-up/scroll-up.component';
+import { ScrollUpComponent } from './components/common-components/scroll-up/scroll-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GuideComponent,
-    ScrollUpComponent
+    ScrollUpComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
