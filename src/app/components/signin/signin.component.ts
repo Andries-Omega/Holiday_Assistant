96608c925d2d782519eadd3655fdd0089ab192c3
Andries-Omega <achimule@jhb.dvt.co.za>
2022-04-07 11:53:59 +0200

    :lipstick: Sign In Form
    added sign in form

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
+  signInForm!: FormGroup;
+  passwordVisible: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.signInForm = this.formBuilder.group({
+      email: [
+        '',
+        [Validators.email, Validators.required, Validators.minLength(1)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(1)]],
+    });
+  }
+  submitSignInReady(): boolean {
+    return this.signInForm.value.email && this.signInForm.value.password;
+  }
+  signInUser() {}
 }
