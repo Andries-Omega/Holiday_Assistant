b85961dac945e852c55b756b25133e5a1c57bf4d
Andries-Omega <achimule@jhb.dvt.co.za>
2022-04-08 09:39:04 +0200

    :construction: App Theme
    remove ngOninit and constructor on child component of switch theme

--- src/app/components/common-components/lightswitch/switchstate/switchstate.component.ts
+++ src/app/components/common-components/lightswitch/switchstate/switchstate.component.ts
@@ -1,17 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-switchstate',
   templateUrl: './switchstate.component.html',
   styleUrls: ['./switchstate.component.scss'],
 })
-export class SwitchStateComponent implements OnInit {
+export class SwitchStateComponent {
   @Input() currentMode!: boolean;
   @Output() newMode = new EventEmitter<boolean>();
-
-  constructor() {}
-
-  ngOnInit(): void {}
 
   switchTheme() {
     this.newMode.emit(!this.currentMode);
